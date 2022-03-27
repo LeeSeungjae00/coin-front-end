9b98fb0230ef49e3bba74de31dd03509614cd8be
LeeSeungjae00 <seungjae266789@gmail.com>
2022-03-27 21:43:59 +0900

    add README

--- src/components/infoPanel/infoPanel.tsx
+++ src/components/infoPanel/infoPanel.tsx
@@ -16,7 +16,7 @@ export default function InfoPanel({ coinMarket, nowPrice, preRise, moneyRise, op
                 💵 {coinMarket}  <p className={nowPrice >= openingPrice ? styles.up : styles.down}>&nbsp;</p>
             </div>
             <div className={styles.marketInfo}>
-                <p><b>✓</b> {coinMarket}은 현재 {nowPrice}₩ 으로 전날보다 {(nowPrice - openingPrice)}{nowPrice >= openingPrice ? "상승중" : "하락중"}이에요.</p>
+                <p><b>✓</b> {coinMarket}은 현재 {nowPrice}₩ 으로 전날보다 {(nowPrice - openingPrice)}{nowPrice >= openingPrice ? "₩ 상승중" : "₩ 하락중"}이에요.</p>
                 <p><b>✓</b> {Math.abs(preRise)}% {preRise > 0 ? "상승 시" : "하락시"} 구매 가능해요</p>
                 <p><b>✓</b> {Math.abs(moneyRise)}₩ {preRise > 0 ? "상승 시" : "하락시"} 구매 가능해요</p>
             </div>
