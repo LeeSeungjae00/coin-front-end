65d5d5e5938a275bb284423c0231c670dccb0b6a
LeeSeungjae00 <seungjae266789@gmail.com>
2022-03-28 21:29:05 +0900

    tradingList 기본틀 작성

--- src/components/tradingList/tradingList.tsx
+++ src/components/tradingList/tradingList.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './tradingList.module.scss'
 
 export default function TradingList() {
     return (
-        <div>
-            
+        <div className={styles.tradingList}>
+            <div className={
+                styles.tradingListBox
+            }></div>
+             <div className={
+                styles.chartBox
+            }></div>
         </div>
     )
 }
--- src/router/routerCoinfig.tsx
+++ src/router/routerCoinfig.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Main from '../components/main'
+import TradingList from '../components/tradingList/tradingList'
 
 
 export default function RouterCoinfig() {
     return (
         <Routes>
             <Route path = "/" element={<Main></Main>}></Route>
+            <Route path = "/tradingList" element={<TradingList></TradingList>}></Route>
         </Routes>
     )
 }
