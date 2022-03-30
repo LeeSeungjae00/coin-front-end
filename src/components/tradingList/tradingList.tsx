739412a20cacf6a3543f0d77e465978d91eac7a7
LeeSeungjae00 <seungjae266789@gmail.com>
2022-03-30 22:40:13 +0900

    무한 스크롤 테스팅 완료

--- src/components/tradingList/tradingList.tsx
+++ src/components/tradingList/tradingList.tsx
@@ -1,7 +1,44 @@
+import axios from 'axios'
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
+import { useInfiniteQuery } from 'react-query'
 import styles from './tradingList.module.scss'
 
 export default function TradingList() {
+    const {ref, inView} = useInView();
+    const {
+        data,
+        error, 
+        fetchNextPage, 
+        hasNextPage, 
+        isFetching,
+        isFetchingNextPage,
+        status
+    } = useInfiniteQuery<any>(
+        "getTradingList",
+        async (_) => {
+            const res = await axios.get('/tradingHistory',{
+                params : {
+                    index : _.pageParam
+                }
+            })
+            return res.data;
+        },
+        {
+            getNextPageParam : (lastPage, _pages) => {
+                return lastPage.nextIndex;
+            }
+        }
+    )
+
+    React.useEffect(() => {
+        console.log(inView)
+        if(inView) fetchNextPage();
+        return () => {}
+    }, [inView])
+
+    if(status === "loading") return <>loading</>
+
     return (
         <div className={styles.tradingList}>
             <div className={
@@ -9,7 +46,19 @@ export default function TradingList() {
             }></div>
              <div className={
                 styles.chartBox
-            }></div>
+            }>
+                {
+                    data && 
+                    data.pages.map(
+                        (val) => val.history.map(
+                            (his : any) => {
+                                return <p
+                                style={{height : "5rem"}}
+                                 key = {his.buyDate}>{his.market}</p>;
+                    }))
+                }
+                <div ref= {ref}></div>
+            </div>
         </div>
     )
 }
