7e3e825fafc30b301715624a973ceb93733476d0
LeeSeungjae00 <seungjae266789@gmail.com>
2022-04-20 22:37:53 +0900

    임시 차트 추가

--- src/components/tradingList/tradingList.tsx
+++ src/components/tradingList/tradingList.tsx
@@ -5,8 +5,62 @@ import useInfinityScroll from '../../hooks/useInfinityScroll'
 import { historyType, tradingHistoryApiType } from '../../types/axiosType'
 import TradingCard from '../tradingCard/tradingCard'
 import styles from './tradingList.module.scss'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
 
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => Math.random()),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => Math.random()),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  
 
 export default function TradingList() {
     const {data, status, RefDiv} = useInfinityScroll((pageParam : String) => {
@@ -50,8 +104,9 @@ export default function TradingList() {
              <div className={
                 styles.chartBox
             }>
-                
+                <Line options={options} data={chartData} />;
             </div>
         </div>
     )
 }
+
