c7b3e063f559ec57bd4d96e90ba15c345dfe453e
LeeSeungjae00 <seungjae266789@gmail.com>
2022-05-14 22:37:53 +0900

    contect Link 태그로 변경

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
             </menu>
             <div className={styles.navButton}>
                 <button className={`${styles.button} ${styles.basic}`}>Login</button>
-                <Link to="/contect" className={`${styles.button} ${styles.colorful}`}>Contect</Link>
+                <Link  to="/contect" className={`${styles.button} ${styles.colorful}`}>Contect</Link>
             </div>
         </div>
     )
