aae1f6235a14671f4062e965db032558599da1db
LeeSeungjae00 <seungjae266789@gmail.com>
2022-05-20 22:37:53 +0900

    [add]기린 svg 추가

--- src/components/Contect/contect.tsx
+++ src/components/Contect/contect.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './contect.module.scss'
+import Giraffe from '../../svgs/giraffe.svg'
 
 export default function Contect() {
     return (
         <div className={styles.contect}>
             <div className={styles.contectBox}>
+                <div className={styles.giraffeBox}>
+                    <img src={Giraffe}></img>
+                </div>
             </div>
         </div>
     )
