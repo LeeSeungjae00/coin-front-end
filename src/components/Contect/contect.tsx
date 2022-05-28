fdb8846092905b3faf2b55eaf1b4e78e0e7843ee
LeeSeungjae00 <seungjae266789@gmail.com>
2022-05-28 22:37:53 +0900

    [ADD] instar SVG ADD

--- src/components/Contect/contect.tsx
+++ src/components/Contect/contect.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './contect.module.scss'
 import Giraffe from '../../svgs/giraffe.svg'
+import GithubSvg from '../../svgs/githubSvg'
+import InstarSvg from '../../svgs/instarSvg'
+import FacebookSvg from '../../svgs/facebookSvg'
 
 export default function Contect() {
     return (
@@ -14,7 +17,18 @@ export default function Contect() {
                     <p>E-Mail. seungjae2668@naver.com</p>
                 </div>
                 <div className={styles.iconBox}>
-                    
+                    <div className={styles.github}>
+                        <a href="www.naver.com">
+                            <GithubSvg></GithubSvg>
+
+                        </a>
+                        <a href="">
+                            <InstarSvg></InstarSvg>
+                        </a>
+                        <a href="">
+                            <FacebookSvg></FacebookSvg>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
