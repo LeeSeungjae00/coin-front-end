92c6961a8ac792f415996a548b08db568b1ccd91
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-04 20:54:54 +0900

    [ADD] tarding history 를 가져오는 fuc 추가

--- src/api/coinApi.ts
+++ src/api/coinApi.ts
@@ -1,8 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getCoinState = (market : string) => () =>
-(axios.get(`/coinState`, {
+export const getCoinState = (market: string) => () =>
+  axios
+    .get(`/coinState`, {
+      params: {
+        market,
+      },
+    })
+    .then((res) => res.data);
+
+export const getTradingHistory = (pageParam: string) =>
+  axios.get("/tradingHistory", {
     params: {
-        market
-    }
-}).then(res => res.data))
+      index: pageParam,
+    },
+  });
