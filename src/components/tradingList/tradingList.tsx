8fbf628efc6e22dcb8ff9ec5c6fa5823163da098
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-05 09:41:34 +0900

    [FIX] 무한 스크롤에서 사용하는 api 함수로 변경한 것 사용

--- src/components/tradingList/tradingList.tsx
+++ src/components/tradingList/tradingList.tsx
@@ -1,66 +1,52 @@
-import axios from 'axios'
-import React from 'react'
-import { InfiniteData } from 'react-query'
-import useChartHook from '../../hooks/useChartHook'
-import useInfinityScroll from '../../hooks/useInfinityScroll'
-import { historyType, tradingHistoryApiType } from '../../types/axiosType'
-import TradingCard from '../tradingCard/tradingCard'
-import styles from './tradingList.module.scss'
-
-
-
-
+import axios from "axios";
+import React from "react";
+import { InfiniteData } from "react-query";
+import { getTradingHistory } from "../../api/coinApi";
+import useChartHook from "../../hooks/useChartHook";
+import useInfinityScroll from "../../hooks/useInfinityScroll";
+import { historyType, tradingHistoryApiType } from "../../types/axiosType";
+import TradingCard from "../tradingCard/tradingCard";
+import styles from "./tradingList.module.scss";
 
 export default function TradingList() {
-    const { data, status, RefDiv } = useInfinityScroll((pageParam: String) => {
-        return axios.get('/tradingHistory', {
-            params: {
-                index: pageParam
-            }
+  const { data, status, RefDiv } = useInfinityScroll((pageParam: String) => {
+    return getTradingHistory(pageParam);
+  });
+  const Chart = useChartHook(data);
+
+  const makeTradingList = React.useCallback(
+    (data: InfiniteData<tradingHistoryApiType>, RefDiv: JSX.Element) => {
+      const tradingList = data.pages.map((val: tradingHistoryApiType) =>
+        val.history.map((his: historyType) => {
+          return (
+            <TradingCard
+              key={his.id}
+              id={his.id}
+              title={his.market}
+              buyTime={his.buyDate}
+              sellTime={his.sellDate}
+              profit={
+                his.sellBalance === null
+                  ? "ing"
+                  : his.sellBalance - his.buyBalance
+              }
+            ></TradingCard>
+          );
         })
-    })
-    const Chart = useChartHook(data)
- 
-    const makeTradingList = React.useCallback(
-        (data: InfiniteData<tradingHistoryApiType>, RefDiv: JSX.Element) => {
-            const tradingList = data.pages.map(
-                (val: tradingHistoryApiType) => val.history.map(
-                    (his: historyType) => {
-                        return <TradingCard
-                            key={his.id} 
-                            id = {his.id}
-                            title={his.market}
-                            buyTime={his.buyDate}
-                            sellTime={his.sellDate}
-                            profit={his.sellBalance === null ?
-                                "ing" :
-                                his.sellBalance - his.buyBalance}
-                        ></TradingCard>
-                    }))
-            return [tradingList, RefDiv]
-        },
-        [],
-    ) 
+      );
+      return [tradingList, RefDiv];
+    },
+    []
+  );
 
-    
+  if (status === "loading") return <>loading</>;
 
-    if (status === "loading") return <>loading</>
-
-    return (
-        <div className={styles.tradingList}>
-            <div className={
-                styles.tradingListBox
-            }>
-                {
-                    data && makeTradingList(data, RefDiv)
-                }
-            </div>
-            <div className={
-                styles.chartBox
-            }>
-                {Chart}
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.tradingList}>
+      <div className={styles.tradingListBox}>
+        {data && makeTradingList(data, RefDiv)}
+      </div>
+      <div className={styles.chartBox}>{Chart}</div>
+    </div>
+  );
 }
-
