0b52f3fb7f7dce6aea17c21f10397263c61e3d95
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-09 11:59:12 +0900

    [ADD] constant 파일에 api 파일 추가 값 입력

---
+++ src/constant/api.ts
@@ -0,0 +1,4 @@
+export const API_GET_COIN_STATE = "/coinState";
+export const API_GET_TRADING_HISTORY = "/tradingHistory";
+export const API_GET_BUY_COIN = "/buyCoin";
+export const API_GET_COIN_LIST = "/todayCoinList";
