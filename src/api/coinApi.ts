73971f3247ab3280aba797232d4df75cd2a3cfd4
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-10 13:29:02 +0900

    [FIX] coinAPI constant api 사용하여 경로명 변경

--- src/api/coinApi.ts
+++ src/api/coinApi.ts
@@ -1,19 +1,25 @@
+import {
+  API_GET_COIN_STATE,
+  API_GET_TRADING_HISTORY,
+  API_GET_BUY_COIN,
+  API_GET_COIN_LIST,
+} from "./../constant/api";
 import axios from "axios";
 
 export const getCoinState = (market: string) =>
-  axios.get(`/coinState`, {
+  axios.get(API_GET_COIN_STATE, {
     params: {
       market,
     },
   });
 
 export const getTradingHistory = (pageParam: String) =>
-  axios.get("/tradingHistory", {
+  axios.get(API_GET_TRADING_HISTORY, {
     params: {
       index: pageParam,
     },
   });
 
-export const getBuyCoin = () => axios.get("/buyCoin");
+export const getBuyCoin = () => axios.get(API_GET_BUY_COIN);
 
-export const getTodayCoinList = () => axios.get("/todayCoinList");
+export const getTodayCoinList = () => axios.get(API_GET_COIN_LIST);
