aa9b0e732b3a922a1c636df398752e69fbbd9701
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-11 15:37:14 +0900

    [FIX] 사용하지 않는 import 삭제

--- src/container/coinState.tsx
+++ src/container/coinState.tsx
@@ -1,18 +1,13 @@
-import axios from 'axios';
-import React, { useEffect } from 'react'
-import { useQuery } from 'react-query';
-import { getCoinState } from '../api/coinApi';
-import InfoPanel from '../components/infoPanel'
-import { useGetCoinState } from '../hooks/queryHooks';
+import React from "react";
+import InfoPanel from "../components/infoPanel";
+import { useGetCoinState } from "../hooks/queryHooks";
 
 interface CoinStateType {
-    market: string
+  market: string;
 }
 
 export default function CoinState({ market }: CoinStateType) {
-    const { data, isLoading, refetch } = useGetCoinState(market);
+  const { data, isLoading, refetch } = useGetCoinState(market);
 
-    return (
-        <InfoPanel {...data}></InfoPanel>
-    )
+  return <InfoPanel {...data}></InfoPanel>;
 }
