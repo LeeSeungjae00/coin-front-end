bad381e80988abb2a49dd3ceaf5d60c0a0b1c2b4
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-12 19:37:14 +0900

    [ADD] react Query 사용한 부분 훅으로 전환

--- src/hooks/queryHooks.tsx
+++ src/hooks/queryHooks.tsx
@@ -1,10 +1,23 @@
 import { useQuery } from "react-query";
-import { getCoinState } from "../api/coinApi";
+import { getBuyCoin, getCoinState, getTodayCoinList } from "../api/coinApi";
+import { todayCoinType } from "../types/axiosType";
 
 export function useGetCoinState(market: string) {
   return useQuery(
     ["coinState", market],
     () => getCoinState(market).then((res) => res.data),
     { refetchInterval: 10000 }
+  );
+}
+
+export function useGetNowBuyCoin() {
+  return useQuery("nowBuyCoin", () => getBuyCoin().then((res) => res.data[0]), {
+    refetchInterval: 10000,
+  });
+}
+
+export function useGetTodayCoinTable() {
+  return useQuery<todayCoinType[], Error>("todayCoin", () =>
+    getTodayCoinList().then((res) => res.data)
   );
 }
