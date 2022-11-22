35808b488eebb1ddd45cb0d709391bf7e6b881ed
LeeSeungjae00 <seungjae266789@gmail.com>
2022-11-23 06:18:17 +0900

    [ADD] form 에 사용할 input 추가

---
+++ src/components/common/input/input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./input.module.scss";
+
+export default function Input() {
+  return (
+    <div className="input" data-lable="test-lable">
+      <input type="text" />
+    </div>
+  );
+}
