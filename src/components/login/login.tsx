52dcbb65edd696229e22c8df4231263f0ecb5626
LeeSeungjae00 <seungjae266789@gmail.com>
2022-11-25 06:12:05 +0900

    [UPDATE] login form 에 따른 디자인 변경

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Input from "../common/input";
+import styles from "./login.module.scss";
 
 export default function Login() {
-  return <div>login</div>;
+  return (
+    <div className={styles.login}>
+      <form className={styles.loginForm}>
+        <Input></Input>
+        <hr />
+      </form>
+    </div>
+  );
 }
