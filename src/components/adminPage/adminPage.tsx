740b7d418fa17e5ebacd6dc31746bf5b84190072
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-04 06:13:22 +0900

    [ADD] strategy card 디자인 초안 완성

--- src/components/adminPage/adminPage.tsx
+++ src/components/adminPage/adminPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./adminPage.module.scss";
+import Giraffe from "../../svgs/giraffe.svg";
 
 export default function AdminPage() {
   return (
@@ -8,9 +9,15 @@ export default function AdminPage() {
         <div className={styles.strategy}>
           <div className={styles.cardHead}>
             <p>1️⃣ BASIC</p>
-            <p>⭐️</p>
+            <p>✔</p>
           </div>
           <hr></hr>
+          <img src={Giraffe} alt="" />
+          <div className={styles.desc}>
+            <p>위험성: ⭐️</p>
+            <p>안정성: ⭐️⭐️⭐️⭐️⭐️</p>
+            <p>수익률: ⭐️</p>
+          </div>
         </div>
         <div className={styles.strategy}></div>
         <div className={styles.strategy}></div>
