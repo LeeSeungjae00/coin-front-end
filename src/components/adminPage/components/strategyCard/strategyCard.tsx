7a6e009c66483e9ec486af047f55e3c52ba8983a
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-06 06:32:19 +0900

    :sparkles: Introduce new features.
    - 분리한 코드 내용 기입하여 추가

--- src/components/adminPage/components/strategyCard/strategyCard.tsx
+++ src/components/adminPage/components/strategyCard/strategyCard.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styles from "./strategyCard.module.scss";
+import Giraffe from "../../svgs/giraffe.svg";
+import lockSvg from "../../svgs/lock.svg";
 
-export default function StrategyCard() {
-  return <div>strategyCard</div>;
+interface StrategyCardProp {
+  name: string;
+  lock: boolean;
+  dangerousness: number;
+  stability: number;
+  yield: number;
+}
+
+export default function StrategyCard(props: StrategyCardProp) {
+  return (
+    <div className={styles.strategy}>
+      <div className={styles.cardHead}>
+        <p>{props.name}</p>
+        {props.lock ? <p>✔</p> : <p>🔒</p>}
+      </div>
+      <hr></hr>
+      <img src={props.lock ? Giraffe : lockSvg} alt="" />
+      <div className={styles.desc}>
+        <p>위험성: {`⭐️`.repeat(props.dangerousness)}</p>
+        <p>안정성: {`⭐️`.repeat(props.stability)}</p>
+        <p>수익률: {`⭐️`.repeat(props.yield)}</p>
+      </div>
+    </div>
+  );
 }
