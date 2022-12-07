aa1c760b77ec941c7660cb50f669099d2966f5f7
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-08 06:10:19 +0900

    :sparkles: Introduce new features.
    - 하한선 상한선 기본적인 기능 추가

--- src/components/adminPage/adminPage.tsx
+++ src/components/adminPage/adminPage.tsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./adminPage.module.scss";
 import Giraffe from "../../svgs/giraffe.svg";
 import StrategyCard from "./components/strategyCard";
 import Slider from "@mui/material/Slider";
-import { Checkbox, FormControlLabel } from "@mui/material";
+import { Alert, Checkbox, FormControlLabel } from "@mui/material";
+
+const marks = [
+  {
+    value: 3,
+    label: "3%",
+  },
+  {
+    value: 10,
+    label: "10%",
+  },
+  {
+    value: 20,
+    label: "20%",
+  },
+  {
+    value: 30,
+    label: "30%",
+  },
+  {
+    value: 100,
+    label: "100%",
+  },
+];
 
 export default function AdminPage() {
+  const [upLine, setUpLine] = useState<{
+    val: number | number[];
+    disable: boolean;
+  }>({
+    val: 10,
+    disable: false,
+  });
+  const [downLine, setDownLine] = useState<{
+    val: number | number[];
+    disable: boolean;
+  }>({
+    val: 10,
+    disable: false,
+  });
+
   return (
     <>
       <div className={styles.strategyList}>
@@ -42,17 +80,75 @@ export default function AdminPage() {
         하한선 (%)
         <div className={styles.flexBox}>
           <div className={styles.sliderContainer}>
-            <Slider></Slider>
+            <Slider
+              disabled={downLine.disable}
+              marks={marks}
+              valueLabelDisplay="auto"
+              value={downLine.val}
+              onChange={(e, value) => {
+                setDownLine({
+                  ...downLine,
+                  val: value,
+                });
+              }}
+              min={3}
+              max={100}
+            ></Slider>
           </div>
-          <FormControlLabel control={<Checkbox />} label="하한선 없음" />
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={(e, checked) => {
+                  setDownLine({
+                    ...downLine,
+                    disable: checked,
+                  });
+                }}
+                value={downLine.disable}
+              />
+            }
+            label="하한선 없음"
+          />
         </div>
+        <Alert sx={{ mb: 3 }}>
+          하한선을 낮출 수록 안정성은 높아지지만 수익률은 낮아질 수 있습니다.
+        </Alert>
         상한선 (%)
         <div className={styles.flexBox}>
           <div className={styles.sliderContainer}>
-            <Slider></Slider>
+            <Slider
+              disabled={upLine.disable}
+              marks={marks}
+              valueLabelDisplay="auto"
+              value={upLine.val}
+              onChange={(e, value) => {
+                setUpLine({
+                  ...upLine,
+                  val: value,
+                });
+              }}
+              min={3}
+              max={100}
+            ></Slider>
           </div>
-          <FormControlLabel control={<Checkbox />} label="상한선 없음" />
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={(e, checked) => {
+                  setUpLine({
+                    ...upLine,
+                    disable: checked,
+                  });
+                }}
+                value={upLine.disable}
+              />
+            }
+            label="상한선 없음"
+          />{" "}
         </div>
+        <Alert>
+          상한선을 낮출 수록 안정성은 낮아지지만 수익률은 높아질 수 있습니다.
+        </Alert>
       </div>
     </>
   );
