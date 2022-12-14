c66f2b2b07950bdb6130548839428dbc367ec98c
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-15 06:30:19 +0900

    :sparkles: Introduce new features.
    - setting UI 완성

--- src/components/setting/setting.tsx
+++ src/components/setting/setting.tsx
@@ -2,7 +2,25 @@ import React from "react";
 import Input from "../common/input";
 import styles from "./setting.module.scss";
 import { FieldValues, useForm } from "react-hook-form";
+import { Alert, Button, TextField } from "@mui/material";
 
 export default function Setting() {
-  return <div className={styles.setting}></div>;
+  return (
+    <div className={styles.setting}>
+      <form className={styles.settingForm}>
+        <TextField
+          value={"123456789010"}
+          label="UPBIT API Token"
+          type={"password"}
+        ></TextField>
+        <TextField
+          value={"qwieajnvnoqijlzksj"}
+          label="Slack API Token"
+          type={"password"}
+        ></TextField>
+        <Alert variant="filled" color="warning"></Alert>
+        <Button>저장</Button>
+      </form>
+    </div>
+  );
 }
