1a9575a0e0e1cfb83a45b57c4aaf198a933ee0c7
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-16 22:40:04 +0900

    :lipstick: Add or update the UI
    - UPBIT 토큰 경고창 추가

--- src/components/setting/setting.tsx
+++ src/components/setting/setting.tsx
@@ -18,7 +18,10 @@ export default function Setting() {
           label="Slack API Token"
           type={"password"}
         ></TextField>
-        <Alert variant="filled" color="warning"></Alert>
+        <Alert variant="filled" color="warning">
+          UPBIT 토큰은 매수 매도 및 코인 정보 가져오기 위함입니다.<br></br>{" "}
+          입금이나 출금에 사용하지 않으므로 해당 기능은 제외 하여도 무방합니다.
+        </Alert>
         <Button>저장</Button>
       </form>
     </div>
